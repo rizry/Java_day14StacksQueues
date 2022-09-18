c122900a6dcc51f391ba6053320646e25aec67ec
rizry <rizzwan.ansarii@gmail.com>
2022-09-18 10:50:18 +0530

    addes nodes to the stack

--- Main.java
+++ Main.java
@@ -5,5 +5,11 @@ public class Main {
   public static void main(String[] args) {
     Stacks<Integer> stk = new Stkacks<>();
 
+    stk.push(70);
+    stk.push(30);
+    stk.push(56);
+    
+    stk.display();
+
   }
 }
